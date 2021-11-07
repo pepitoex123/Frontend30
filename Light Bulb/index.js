4ef04a29208ea8d07791f1d105b07a144867253a
Uriel Cabrera <urimartincabrera123@gmail.com>
2021-11-06 22:25:56 -0300

    added light-bulb and desk svg

---
+++ Light Bulb/index.js
@@ -0,0 +1,9 @@
+const light = document.querySelector(".c-bulb");
+
+
+const toggleLight = () => {
+    document.querySelector("body").classList.toggle("is-light-on");
+}
+
+light.addEventListener("mouseover",toggleLight)
+light.addEventListener("mouseout",toggleLight)
